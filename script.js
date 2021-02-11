abc3fde2dd7354f9e1dae488b5a6029d48be0ec3
nicolewallace09 <nicole.elisaw@gmail.com>
2021-02-10 20:13:14 -0800

    connected input and erase

--- script.js
+++ script.js
@@ -1,88 +1,90 @@
 const answer = () => {
-    // let userQuestion = '';
+    let question = document.getElementById("question"); 
+
     let randomNumber = Math.floor(Math.random() * 18);
     let eightBall = randomNumber;
  
-    switch(eightBall) {
-    case 0:
-    eightBall = 'it is certain';
-    break;
+    if(question.value.length > 1) {
+        switch(eightBall) {
+        case 0:
+        eightBall = 'it is certain';
+        break;
 
-    case 1:
-    eightBall = 'it is decidedly so';
-    break;
+        case 1:
+        eightBall = 'it is decidedly so';
+        break;
 
-    case 2:
-    eightBall = 'without a doubt';
-    break;
+        case 2:
+        eightBall = 'without a doubt';
+        break;
 
-    case 3:
-    eightBall = 'yes, definitely';
-    break;
+        case 3:
+        eightBall = 'yes, definitely';
+        break;
 
-    case 4:
-    eightBall = 'you may rely on it';
-    break;
+        case 4:
+        eightBall = 'you may rely on it';
+        break;
 
-    case 5:
-    eightBall = 'as i see it, yes';
-    break; 
+        case 5:
+        eightBall = 'as i see it, yes';
+        break; 
 
-    case 6:
-    eightBall = 'most likely';
-    break; 
+        case 6:
+        eightBall = 'most likely';
+        break; 
 
-    case 7:
-    eightBall = 'outlook good';
-    break;
+        case 7:
+        eightBall = 'outlook good';
+        break;
 
-    case 8:
-    eightBall = 'signs point to yes';
-    break;
+        case 8:
+        eightBall = 'signs point to yes';
+        break;
 
-    case 9:
-    eightBall = 'reply hazy';
-    break;
+        case 9:
+        eightBall = 'reply hazy';
+        break;
 
-    case 10:
-    eightBall = 'try again';
-    break;
+        case 10:
+        eightBall = 'try again';
+        break;
 
-    case 11:
-    eightBall = 'ask again later';
-    break;
+        case 11:
+        eightBall = 'ask again later';
+        break;
 
-    case 12:
-    eightBall = 'better not tell you now';
-    break;
+        case 12:
+        eightBall = 'better not tell you now';
+        break;
 
-    case 13:
-    eightBall = 'cannot predict now';
-    break;
+        case 13:
+        eightBall = 'cannot predict now';
+        break;
 
-    case 14:
-    eightBall = 'concentrate and ask again';
-    break;
+        case 14:
+        eightBall = 'concentrate and ask again';
+        break;
 
-    case 15:
-    eightBall = "don't count on it";
-    break;
+        case 15:
+        eightBall = "don't count on it";
+        break;
 
-    case 16:
-    eightBall = 'my reply is no';
-    break;
+        case 16:
+        eightBall = 'my reply is no';
+        break;
 
-    case 17:
-    eightBall = 'my sources say no';
-    break;
+        case 17:
+        eightBall = 'my sources say no';
+        break;
 
-    case 18:
-    eightBall = 'outlook not so good';
-    break;
+        case 18:
+        eightBall = 'outlook not so good';
+        break;
 
-    case 19:
-    eightBall = 'very doubtful';
-    break;
+        case 19:
+        eightBall = 'very doubtful';
+        break;
     }
 
     // displaying answer. call when during shake(); 
@@ -91,6 +93,17 @@ const answer = () => {
     message.setAttribute('id', 'answerText')
     message.innerHTML = eightBall;
     displayAnswer.appendChild(message); 
+
+     // remove if there is a previous question 
+    let previousQuestion = document.getElementById("question")
+        if(previousQuestion != null){
+            previousQuestion.value = '';
+     }
+    
+
+    } else {
+        window.alert("Please ask a question")
+    }
 }
 
 
